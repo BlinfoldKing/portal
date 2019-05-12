21d7fc980edc79c9492f272eb28c0633875bf9e6
Ganesha Danu <ganeshadanu1@gmail.com>
2019-05-12 23:20:22 +0700

    add redirect operation

--- lib/portal/router.ex
+++ lib/portal/router.ex
@@ -3,22 +3,24 @@ defmodule Portal.Router do
   alias Portal.Repo
   alias Portal.Links
 
+  
+
   plug :match
   plug :dispatch
 
   post "/:key/to/:url" do
     changeset  = %Links{ key: key, url: url }
     Repo.insert(changeset)
-    IO.inspect Repo.all(Links)
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(200, Poison.encode!(%{ key: key, url: url }))
   end
 
   get "/:key" do
+    data = Repo.get(Links, key)
     conn
     |> Plug.Conn.resp(:found, "")
-    |> Plug.Conn.put_resp_header("location", "https://google.com")
+    |> Plug.Conn.put_resp_header("location", "https://#{data.url}")
   end
 
   get "/g/:key" do
